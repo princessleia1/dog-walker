21d96e610ff9efb200b5a43d1effad3ed4486356
Kirstene Groth <groth3@qut.edu.au>
2019-11-18 10:03:40 +1000

    Minor update index.js

--- app/index.js
+++ app/index.js
@@ -1,9 +1,8 @@
 import { Application } from "./lib/view";
-
 import { ViewEnd } from "./views/end";
 import { ViewExercise } from "./views/exercise";
 import { ViewSelect } from "./views/select";
-import { ViewTips } from "./views/tips";
+import { ViewTips } from "./views/tips";  // New view
 
 class MultiScreenApp extends Application {
   screens = { ViewSelect, ViewExercise, ViewTips, ViewEnd };
